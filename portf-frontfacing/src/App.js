85fa0be92ebfe90e47149109a136f05be4364c15
EastAsianInc <113070877+EastAsianInc@users.noreply.github.com>
2023-08-12 14:19:31 -0400

    Some more structure, babel dependency fix

--- portf-frontfacing/src/App.js
+++ portf-frontfacing/src/App.js
@@ -1,5 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './scenes/HomePage/homepage';
+import ProjectsPage from './scenes/ProjectsPage/projectspage';
 
 function App() {
   return (
@@ -18,7 +23,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+          {/* <Route/> */}
+          <Route exact path='/' Component={HomePage}/>
+          <Route path='/about' Component={ProjectsPage}/>
+        </Routes>
+      </Router>
     </div>
+
   );
 }
 
